84c1db5bda1188399991265c0fe955126e18b9f9
sandeep sharma <sandeep.sharma@trantorinc.com>
2017-11-24 18:25:45 +0530

    Initial commits

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var redis = require('redis');
+
+var client = redis.createClient(); // will use 127.0.0.1 and 6379
+
+client.on('connect', function() {
+    console.log('Redis connected');
+});
 
 // Test Client
 app.get('/client', function(req, res){
   res.sendFile(__dirname + '/public/client.html');
 });
 
+// Connect Socket IO
 io.on('connection', function(socket){
   console.log('a user connected');
 });
